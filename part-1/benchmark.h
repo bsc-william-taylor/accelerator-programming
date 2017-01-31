3e73cf7beb058965760cc607022155700508d222
William Taylor <wi11berto@yahoo.co.uk>
2017-01-31 17:46:14 +0000

    finished working version that is faster than the given example + better folder names

---
+++ part-1/benchmark.h
@@ -0,0 +1,38 @@
+
+#pragma once
+
+#include <fstream>
+#include <ctime>
+
+enum class measure_in
+{
+    secs = 1000,
+    ms = 1
+};
+
+template<measure_in measure, unsigned times, typename Functor, typename... Args>
+void benchmark(Functor&& method, Args&&... args)
+{
+    auto typeString = measure == measure_in::ms ? " (ms) " : " (secs) ";
+    auto total = 0.0;
+
+    std::ofstream csvFile("benchmark.csv");
+    csvFile << "Benchmark Results \n";
+    csvFile << "ID, Time" << typeString << ", \n";
+    
+    for (auto i = 1u; i <= times; ++i) 
+    {
+        const auto start = clock();
+        method(std::forward<Args>(args)...);
+        const auto stop = clock();
+
+        auto time = (stop - start) / double(measure);
+        total += time;
+
+        csvFile << i << "," << time << "\n";
+    }
+
+    csvFile << "Total," << typeString << "Average" << typeString << "\n";
+    csvFile << total << "," << total / times;
+    csvFile.close();
+}
