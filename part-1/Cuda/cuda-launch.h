1ffa1ebfe04846ff537fec24d71d10ae71b86c3b
William Taylor <wi11berto@yahoo.co.uk>
2017-02-01 19:12:29 +0000

    addressing TODOs #4

---
+++ part-1/Cuda/cuda-launch.h
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include <cuda_runtime.h>
+#include <algorithm>
+
+namespace cuda
+{
+    struct launchInfo
+    {
+        int blocks, threads, width, height;
+    };
+
+    struct workload
+    {
+        int offset, size, stride;
+    };
+
+    template<typename K, typename... Args>
+    cudaError_t start(K kernel, launchInfo& launch, Args&&... args)
+    {
+        kernel << <launch.blocks, launch.threads >> >(launch, std::forward<Args>(args)...);
+        return cudaDeviceSynchronize();
+    }
+
+    inline __device__ workload allocateWork(cuda::launchInfo& info, int block, int thread)
+    {
+        auto blockSizeY = info.height / info.blocks;
+        auto blockSize = info.width * blockSizeY;
+        auto threadSize = blockSize / info.threads;
+
+        auto offset = block * blockSize + thread * threadSize;
+        auto stride = block * blockSizeY + thread * blockSizeY / info.threads;
+
+        return { offset, offset + threadSize, stride };
+    }
+}
