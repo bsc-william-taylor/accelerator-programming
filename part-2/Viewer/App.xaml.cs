ef723aef640416d472d02719cee682012435946a
william-taylor <wi11berto@yahoo.co.uk>
2017-03-07 13:46:00 +0000

    basic work on viewer app

--- part-2/Viewer/App.xaml.cs
+++ part-2/Viewer/App.xaml.cs
@@ -1,17 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿using System.Windows;
+using System.IO;
+using System.Drawing;
+using System.ComponentModel;
+using System;
+using System.Text;
+using System.Windows.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-
 namespace Viewer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static string GetByte(BinaryReader reader, StringBuilder builder)
+        {
+            while (reader.BaseStream.Position != reader.BaseStream.Length)
+            {
+                char temp = reader.ReadChar();
+
+                while(reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    if (temp == '\n' && builder.Length == 0)
+                        temp = reader.ReadChar();
+                    else
+                        break;
+                }
+
+                if (temp != ' ' && temp != '\n')
+                    builder.Append(temp);
+                else
+                    break;
+            }
+
+            var str = builder.ToString();
+            builder.Clear();
+            return str;
+        }
+
+        public static Bitmap ReadBitmapFromPPM(string file, BackgroundWorker worker)
+        {
+            worker.ReportProgress(0);
+
+            var builder = new StringBuilder();
+            var reader = new BinaryReader(new FileStream(file, FileMode.Open));
+            var magic = GetByte(reader, builder);
+            var width = int.Parse(GetByte(reader, builder));
+            var height = int.Parse(GetByte(reader, builder));
+            var maxValue = int.Parse(GetByte(reader, builder));
+            var totalWork = width * height;
+
+            Bitmap bitmap = new Bitmap(width, height);
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    var progress = (double)(width * y + x) / (double)totalWork;
+                    var r = int.Parse(GetByte(reader, builder));
+                    var g = int.Parse(GetByte(reader, builder));
+                    var b = int.Parse(GetByte(reader, builder));
+
+                    bitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    worker.ReportProgress((int)(progress * 100.0));
+                }                
+            }
+
+            return bitmap;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var processors = Environment.ProcessorCount;
+
+            MessageBox.Show(processors.ToString());
+        }
     }
 }
--- part-2/Viewer/MainWindow.xaml.cs
+++ part-2/Viewer/MainWindow.xaml.cs
@@ -1,28 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using Forms = System.Windows.Forms;
+using System.ComponentModel;
 
 namespace Viewer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ViewImage(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            
+            if(fileDialog.ShowDialog() == Forms.DialogResult.OK)
+            {
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.WorkerReportsProgress = true;
+                worker.DoWork += LoadImage;
+                worker.ProgressChanged += Worker_ProgressChanged;
+                worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+                worker.RunWorkerAsync(fileDialog.FileName);
+            }
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ImageView.Source = (BitmapImage)e.Result;
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Progress.Value = e.ProgressPercentage;
+        }
+
+        private void LoadImage(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            var filename = e.Argument.ToString();
+            var bitmap = App.ReadBitmapFromPPM(filename, sender as BackgroundWorker);
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                e.Result = bitmapImage;
+
+                worker.ReportProgress(100);
+            }
+        }
+
+        private void Run(object sender, RoutedEventArgs e)
+        {
+            Forms.MessageBox.Show("Run");
+        }
+
+        private void Settings(object sender, RoutedEventArgs e)
+        {
+            Forms.MessageBox.Show("Settings");
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            Forms.MessageBox.Show("About");
         }
     }
 }
